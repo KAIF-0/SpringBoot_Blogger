56cc1f1abb387a060cb20bef569ba85aa4cc0f3a
kaif-0 <kaif8700979251@gmail.com>
2025-09-28 20:46:17 +0530

    lombok for setters and getters

--- src/main/java/com/blogger/controller/BloggerController.java
+++ src/main/java/com/blogger/controller/BloggerController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 import com.blogger.entity.BlogEntity;
 import com.blogger.service.BlogService;
@@ -22,40 +24,74 @@ import com.blogger.service.BlogService;
 public class BloggerController {
     private Map<Long, BlogEntity> blogStore = new HashMap<>();
 
-    //can be used with autowired/auto injection also
+    // can be used with autowired/auto injection also
     @Autowired
     private BlogService blogService;
 
     // private final BlogService blogService;
 
     // public BloggerController(BlogService blogService) {
-    //     this.blogService = blogService;
+    // this.blogService = blogService;
     // }
 
     @GetMapping("/getAllBlogs")
-    public BlogEntity[] getAllBlogs() {
-        return blogService.getAllBlogs();
+    public ResponseEntity<BlogEntity[]> getAllBlogs() {
+        try {
+            BlogEntity[] blogs = blogService.getAllBlogs();
+            return new ResponseEntity<>(blogs, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping("/createBlog")
-    public BlogEntity createBlog(@RequestBody BlogEntity blog) {
-        BlogEntity createdBlog = blogService.addBlog(blog.getTitle(), blog.getContent(), blog.getAuthor());
-        return createdBlog;
+    public ResponseEntity<?> createBlog(@RequestBody BlogEntity blog) {  //question marks here means anything as body
+        try {
+            BlogEntity createdBlog = blogService.addBlog(blog.getTitle(), blog.getContent(), blog.getAuthor());
+            return new ResponseEntity<>("Blog Created Successfully!", HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/get/{id}")
-    public BlogEntity getBlogById(@PathVariable Long id) {
-        return blogService.getBlogById(id);
+    public ResponseEntity<BlogEntity> getBlogById(@PathVariable Long id) {
+        try {
+            BlogEntity blog = blogService.getBlogById(id);
+            if (blog != null) {
+                return new ResponseEntity<>(blog, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public BlogEntity deleteBlogById(@PathVariable Long id) {
-        return blogService.deleteBlogById(id) ;
+    public ResponseEntity<BlogEntity> deleteBlogById(@PathVariable Long id) {
+        try {
+            BlogEntity deletedBlog = blogService.deleteBlogById(id);
+            if (deletedBlog != null) {
+                return new ResponseEntity<>(deletedBlog, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping("/update/{id}")
-    public BlogEntity updateBlogById(@PathVariable Long id, @RequestBody BlogEntity updatedBlog) {
-        return blogService.updateBlog(id, updatedBlog.getTitle(), updatedBlog.getContent(), updatedBlog.getAuthor());
+    public ResponseEntity<BlogEntity> updateBlogById(@PathVariable Long id, @RequestBody BlogEntity updatedBlog) {
+        try {
+            BlogEntity blog = blogService.updateBlog(id, updatedBlog.getTitle(), updatedBlog.getContent(),
+                    updatedBlog.getAuthor());
+            if (blog != null) {
+                return new ResponseEntity<>(blog, HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
--- src/main/java/com/blogger/entity/BlogEntity.java
+++ src/main/java/com/blogger/entity/BlogEntity.java
@@ -2,9 +2,19 @@ package com.blogger.entity;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Entity
 @Table(name = "blogs")
+// @Getter  //used lombok to avoid boilerplate code of getters and setters
+// @Setter
+// @NoArgsConstructor
+// @AllArgsConstructor
+@Data  //combination of @Getter, @Setter, @RequiredArgsConstructor, @ToString, and @EqualsAndHashCode
 public class BlogEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,9 +32,9 @@ public class BlogEntity {
     @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 
-    public BlogEntity() {
-        this.createdAt = LocalDateTime.now();
-    }
+    // public BlogEntity() {
+    //     this.createdAt = LocalDateTime.now();
+    // }
 
     public BlogEntity(String title, String content, String author) {
         this.title = title;
@@ -33,44 +43,44 @@ public class BlogEntity {
         this.createdAt = LocalDateTime.now();
     }
 
-    // getter and setter methods
-    public Long getId() {
-        return id;
-    }
+    // // getter and setter methods
+    // public Long getId() {
+    //     return id;
+    // }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    // public void setId(Long id) {
+    //     this.id = id;
+    // }
 
-    public String getTitle() {
-        return title;
-    }
+    // public String getTitle() {
+    //     return title;
+    // }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    // public void setTitle(String title) {
+    //     this.title = title;
+    // }
 
-    public String getContent() {
-        return content;
-    }
+    // public String getContent() {
+    //     return content;
+    // }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+    // public void setContent(String content) {
+    //     this.content = content;
+    // }
 
-    public String getAuthor() {
-        return author;
-    }
+    // public String getAuthor() {
+    //     return author;
+    // }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+    // public void setAuthor(String author) {
+    //     this.author = author;
+    // }
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
+    // public LocalDateTime getCreatedAt() {
+    //     return createdAt;
+    // }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+    // public void setCreatedAt(LocalDateTime createdAt) {
+    //     this.createdAt = createdAt;
+    // }
 }
